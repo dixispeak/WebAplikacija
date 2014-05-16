f9d057af459f8995334f3af6262685f06dce7df0
Monika <monika.staniute@outlook.com>
2014-05-16 23:00:39 +0300

    Pirmas Commitas

---
+++ WebAplikacija/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAplikacija.Models;
+
+namespace WebAplikacija.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Pirkiniai sarasas = new Pirkiniai();
+            sarasas.PirkiniuSarasas = sarasas.ReadDocument();
+            return View(sarasas);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pirkiniai sarasas)
+        {
+            //ModelState.Clear();
+            //sarasas.Name = "asda";
+            sarasas.WriteToDocument(sarasas.Name);
+            sarasas.PirkiniuSarasas = sarasas.ReadDocument();
+            return View(sarasas);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ WebAplikacija/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAplikacija.Startup))]
+namespace WebAplikacija
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
